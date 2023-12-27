eaa64ba3ba691e45e792c5c887e35b14c8671e8a
Danila Duchitsky <danila.f1n1@yandex.ru>
2023-12-28 01:19:09 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,24 @@
 const cardContainer = document.querySelector(".places__list");
 
-// Создаем карточки
-const createCard = (initialCards, deleteCard) => {
+const createCard = (cardData, deleteCard) => {
   const template = document.querySelector("#card-template").content;
+  const card = template.querySelector(".card").cloneNode(true);
+  const cardImage = card.querySelector(".card__image");
+  const cardTitle = card.querySelector(".card__title");
 
-  // Проходимся по входному массиву
-  const cardTemplate = initialCards.map((item) => {
-    const card = template.querySelector(".card").cloneNode(true);
-    const cardImage = card.querySelector(".card__image");
-    const cardTitle = card.querySelector(".card__title");
+  cardImage.src = cardData.link;
+  // Во входных данных нет alt у картинок, так что будет name, но когда появится alt, то будет alt
+  cardImage.alt = cardData.alt || cardData.name;
+  cardTitle.textContent = cardData.name;
 
-    // Подставляем значения из объекта item
-    cardImage.src = item.link;
-    cardTitle.textContent = item.name;
+  const cardDeleteButton = card.querySelector(".card__delete-button");
+  cardDeleteButton.addEventListener("click", (e) => deleteCard(e));
 
-    // Слушаем клики на кнопке
-    const cardDeleteButton = card.querySelector(".card__delete-button");
-    cardDeleteButton.addEventListener("click", (e) => deleteCard(e));
-
-    // Записываем карточку в cardTemplate
-    return card;
-  });
-
-  // Возвращаем готовый массив из всех карточек с нужными значениями внутри
-  return cardTemplate;
+  return card;
 };
 
-// При клике на корзину удялем нужную карточку
+const renderCard = (card) => cardContainer.append(card);
+
 const deleteCard = (e) => e.target.closest("li").remove();
 
-// Рендерим полный список карточек разворачивая массив, возвращающий разметку
-cardContainer.append(...createCard(initialCards, deleteCard));
+initialCards.forEach((card) => renderCard(createCard(card, deleteCard)));
